abf14a0a354948285b028f987203be6a339fc55b
E-r-i-c2 <lei.eric12@gmail.com>
2025-02-02 09:41:20 +0800

    initnial jvreiuen

---
+++ iphonexvi/iphonexviApp.swift
@@ -0,0 +1,17 @@
+//
+//  iphonexviApp.swift
+//  iphonexvi
+//
+//  Created by Lei Boyang on 2/2/25.
+//
+
+import SwiftUI
+
+@main
+struct iphonexviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iphonexviTests/iphonexviTests.swift
@@ -0,0 +1,16 @@
+//
+//  iphonexviTests.swift
+//  iphonexviTests
+//
+//  Created by Lei Boyang on 2/2/25.
+//
+
+import Testing
+
+struct iphonexviTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
